81ff68bc084dfaa5455550d72270457d3e0add12
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2022-08-23 13:17:38 +0100

    union storage

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
-#include "instance_of.h"
+#include "union_storage.h"
 
 #include <vector>
 
 namespace
 {
-
+namespace instance_of_test {
 static_assert(tools::instance_of<std::vector<int>, std::vector>);
 static_assert(!tools::instance_of<int, std::vector>);
 static_assert(!tools::instance_of<std::vector<int>&, std::vector>);
+} // namespace instance_of_test
+
+namespace union_storage_test {
+
+using us = tools::union_storage<int, char, double>;
+
+static_assert(std::same_as<int const& , decltype(get<0>(std::declval<us const& >()))>);
+static_assert(std::same_as<int&,        decltype(get<0>(std::declval<us&       >()))>);
+static_assert(std::same_as<int&&,       decltype(get<0>(std::declval<us&&      >()))>);
+static_assert(std::same_as<int const&&, decltype(get<0>(std::declval<us const&&>()))>);
+
+static_assert(std::same_as<char const& , decltype(get<1>(std::declval<us const& >()))>);
+static_assert(std::same_as<char&,        decltype(get<1>(std::declval<us&       >()))>);
+static_assert(std::same_as<char&&,       decltype(get<1>(std::declval<us&&      >()))>);
+static_assert(std::same_as<char const&&, decltype(get<1>(std::declval<us const&&>()))>);
+
+static_assert(std::same_as<double const& , decltype(get<2>(std::declval<us const& >()))>);
+static_assert(std::same_as<double&,        decltype(get<2>(std::declval<us&       >()))>);
+static_assert(std::same_as<double&&,       decltype(get<2>(std::declval<us&&      >()))>);
+static_assert(std::same_as<double const&&, decltype(get<2>(std::declval<us const&&>()))>);
+
+
+}  // namespace union_storage_test
 
 } // namespace
 
--- union_storage.h
+++ union_storage.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "instance_of.h"
+#include "fwd.h"
+#include <utility>
+
 namespace tools {
 
 template <typename T, typename ...Ts>
-struct union_storage
-{
+struct union_storage {
   union {
     T head;
     union_storage<Ts...> tail;
@@ -12,12 +15,27 @@ struct union_storage
 };
 
 template <typename T>
-struct union_storage<T>
-{
+struct union_storage<T> {
   T head;
 };
 
-template <std::size_t i, typename ...Ts>
-auto&& get(auto&& self)
+template <typename >
+struct union_storage_size;
+
+template <typename ... Ts>
+struct union_storage_size<union_storage<Ts...>> :
+   std::integral_constant<std::size_t, sizeof...(Ts)> {
+};
+
+template <typename T>
+constexpr std::size_t union_storage_size_v = union_storage_size<T>::value;
+
+template <std::size_t i, typename US>
+  requires instance_of<std::remove_cvref_t<US>, union_storage> &&
+           (i < union_storage_size_v<std::remove_cvref_t<US>>)
+auto&& get(US&& self) {
+  if constexpr (i == 0) return TOOLS_FWD(self).head;
+  else                  return get<i - 1>(TOOLS_FWD(self).tail);
+}
 
 }  // namespace tools
